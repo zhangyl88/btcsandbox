5d03bb53fd4c53103183a76b29fe5cc781603c4d
dycode999 <dycode999@gmail.com>
2022-07-27 11:12:36 -1200

    Set up

--- account/admin.py
+++ account/admin.py
@@ -1,3 +1,39 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
 
-# Register your models here.
+from account.models import *
+
+
+# Custom Admin - Account
+class AccountAdmin(UserAdmin):
+    list_display            = ('email', 'username', 'is_confirmed', 'ip_address',)
+    readonly_fields         = ('id', 'date_joined', 'last_login',)
+    search_fields           = ('email', 'username', 'ip_address', 'id',)
+
+    ordering                = [('-date_joined'),]
+
+    filter_horizontal       = ()
+    list_filter             = ('is_active', 'is_confirmed', 'date_joined',)
+  
+    fieldsets               = (
+        ('Basic Informations', {'fields': ('email', 'username', 'fullname',)}),
+
+        ('Security Required', {'fields': ('password',)}),
+
+        ('Auto Generated', {'fields': ('ip_address',)}),
+
+        ('Indicators', {'fields': ('is_admin', 'is_active', 'is_confirmed', 'is_superuser', 'is_staff',)}),
+        
+        ('Read-Only', {'fields': ('id', 'date_joined', 'last_login',)}),
+    )
+
+    add_fieldsets = (
+        (None, {
+            'classes': ('wide',),
+            'fields': ('email', 'username', 'passcode', 'password1', 'password2',)
+        }),
+    )
+
+
+# Model Registrations
+admin.site.register(Account, AccountAdmin)
--- account/models.py
+++ account/models.py
@@ -1,3 +1,103 @@
-from django.db import models
+import socket
+import requests
 
-# Create your models here.
+from django.db import models
+from django.contrib.auth.models import PermissionsMixin
+from django.contrib.auth.models import AbstractBaseUser, BaseUserManager
+from django.dispatch import receiver
+from django.conf import settings
+from django.db.models.signals import post_save
+from django.utils.translation import gettext_lazy as _
+
+from rest_framework.authtoken.models import Token
+
+# Account Manager
+class AccountManager(BaseUserManager):
+    def create_user(self, username, email, password=None, **other_fields):
+        if not email:
+            raise ValueError('Email is a required user field')
+
+        if not username:
+            raise ValueError('Username is a required user field')
+
+        email = self.normalize_email(email)
+        username = username.lower()
+        
+        user = self.model(
+            username = username,
+            email    = email, 
+            **other_fields
+        )
+
+        user.set_password(password)
+        user.save(using=self.db)
+
+        return user
+
+    def create_superuser(self, username, email, password=None, **other_fields):
+        other_fields.setdefault('is_staff', True)
+        other_fields.setdefault('is_active', True)
+        other_fields.setdefault('is_superuser', True)
+        other_fields.setdefault('is_admin', True)
+        other_fields.setdefault('is_confirmed', True)
+
+        return self.create_user(username, email, password, **other_fields)
+
+
+# Account
+class Account(AbstractBaseUser, PermissionsMixin):
+    # basic informations
+    username                = models.CharField(_('Username'), max_length=50, unique=True)
+    email                   = models.EmailField(_('Email'), unique=True)
+    fullname                = models.CharField(_('Full Name'), max_length=100, blank=True, null=True)
+
+
+    # Auto Generated
+    ip_address              = models.GenericIPAddressField(_('Ip Address'), blank=True, null=True)
+
+    #~ Overrides
+    date_joined             = models.DateTimeField(verbose_name='date joined', auto_now_add=True)
+    last_login              = models.DateTimeField(verbose_name='last login', auto_now=True)
+
+    # Indicators
+    is_admin                = models.BooleanField(default=False)
+    is_active               = models.BooleanField(default=True)
+    is_confirmed            = models.BooleanField(default=False)
+    is_superuser            = models.BooleanField(default=False)
+    is_staff                = models.BooleanField(default=False)
+
+    USERNAME_FIELD          = 'email'
+    REQUIRED_FIELDS         = ['username']
+
+    objects                 = AccountManager()
+
+    class Meta:
+        managed = True
+        verbose_name = 'Account'
+        verbose_name_plural = 'Accounts'
+
+    def __str__(self):
+        return self.username
+
+    def has_perm(self, perm, obj=None):
+        return self.is_admin
+
+    def has_module_perms(self, app_label):
+        return True
+    
+
+# Generates token for registered user
+@receiver(post_save, sender=settings.AUTH_USER_MODEL)
+def generator(sender, instance, created=False, **kwargs):
+    if created:
+        Token.objects.create(user=instance)
+
+        try:
+            # Grabbing User IP Address
+            ip_addr = requests.get('https://api.ipify.org/').text
+            instance.ip_address = ip_addr
+        
+        except:
+            instance.ip_address = socket.gethostbyname('localhost')
+
+        instance.save()
