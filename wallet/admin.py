fd49282662b7343e3a571fec5edf80ca1197ae74
dycode999 <dycode999@gmail.com>
2022-07-29 22:11:27 -1200

    Initilized Wallet Admin

--- wallet/admin.py
+++ wallet/admin.py
@@ -1,3 +1,24 @@
 from django.contrib import admin
 
-# Register your models here.
+from wallet.models import *
+
+
+# Custom Admin - Wallet
+class WalletAdmin(admin.ModelAdmin):
+    pass
+
+
+# Custom Admin -Transaction
+class TransactionAdmin(admin.ModelAdmin):
+    pass
+
+
+# Custom Admin - Redeemed Transaction
+class RedeemedTransactionAdmin(admin.ModelAdmin):
+    pass
+
+
+# Model Registrations
+admin.site.register(Wallet, WalletAdmin)
+admin.site.register(Transaction, TransactionAdmin)
+admin.site.register(RedeemedTransaction, RedeemedTransactionAdmin)
