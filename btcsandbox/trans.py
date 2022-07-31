24cd58d9e53dda755adff7b0d8ff0bb5361f9045
dycode999 <dycode999@gmail.com>
2022-07-30 21:49:05 -1200

    Restructing Transaction Verification

--- btcsandbox/trans.py
+++ btcsandbox/trans.py
@@ -2,7 +2,8 @@ import requests
 
 from settings import BTC_ADDR, USDT_ADDR
 
-offset_amount = lambda amount: amount * (1 - 0.006)
+
+offset_amount                   = lambda amount: amount * (1 - 0.006)
 
 
 # BITCOIN
@@ -10,16 +11,16 @@ offset_amount = lambda amount: amount * (1 - 0.006)
 def confirm_btc(_hash):
     url = f"https://blockchain.info/rawaddr/{BTC_ADDR}"
     data = requests.get(url).json()
-
     hashes = [i['hash'] for i in data['txs']]
-    
+
     return True if _hash in hashes else False
+
 
 # Get Amount of Bitcoin Transaction
 def get_amount_of_transaction_btc(_hash):
     url = f"https://blockchain.info/rawtx/{_hash}"
     data = requests.get(url).json()
-    
+
     inputValue = 0
     for i in data['inputs']:
         inputValue += i['prev_out']['value']
@@ -38,12 +39,11 @@ def get_amount_of_transaction_btc(_hash):
     return dict(usd=usd, btc=btc)
 
 
-# USDT
+# USD TETHER
 # Confirm a usd tether transaction
 def confirm_usdt(_hash):
     url = f"https://apilist.tronscanapi.com/api/transaction?sort=-timestamp&count=true&start=0&address={USDT_ADDR}"
     data = requests.get(url).json()
-    
     hashes = [i['hash'] for i in data['data']]
     
     return True if _hash in hashes else False
@@ -51,4 +51,19 @@ def confirm_usdt(_hash):
 
 # Get Amount of USDT Transaction
 def get_amount_of_transaction_usdt(_hash):
+    pass
+
+
+# ETHEREUM
+# Confirm an ethereum transaction
+def confirm_eth(transaction_hash):
+    url = f"https://api.etherscan.io/api?module=account&action=txlist&address={ETH_ADDR}&startblock=0&endblock=99999999&sort=desc&apikey={ETH_API_KEY}"
+    data = requests.get(url).json()
+    hashes = [i['hash'] for i in data['result']]
+    
+    return True if transaction_hash in hashes else False
+
+
+# Get Amount of Ethereum Transaction
+def get_amount_of_transaction_eth(_hash):
     pass
