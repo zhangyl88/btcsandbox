a203fe5a1e9efcaaf008c62b02554dd7ec557942
dycode999 <dycode999@gmail.com>
2022-07-31 10:06:21 -1200

    Convert to ETH

--- btcsandbox/trans.py
+++ btcsandbox/trans.py
@@ -1,6 +1,6 @@
 import requests
 
-from settings import BTC_ADDR, USDT_ADDR
+from settings import BTC_ADDR, USDT_ADDR, ETH_ADDR, ETH_API_KEY
 
 
 offset_amount                   = lambda amount: amount * (1 - 0.006)
@@ -65,5 +65,20 @@ def confirm_eth(transaction_hash):
 
 
 # Get Amount of Ethereum Transaction
-def get_amount_of_transaction_eth(_hash):
-    pass
+def get_ethereum_transaction_details(transaction_hash):
+    url = f"https://api.etherscan.io/api?module=account&action=txlist&address={ETH_ADDR}&startblock=0&endblock=99999999&sort=desc&apikey={ETH_API_KEY}"
+    data = requests.get(url).json()
+    
+    for i in data['result']:
+        if i['hash'] == transaction_hash:
+            amount = int(i['value']) * pow(10, -18)
+            def convert_ethereum_to_usd(amount):
+                url = "https://api.alternative.me/v2/ticker/?convert=USD"
+                data = requests.get(url).json()
+                
+                return amount * data['data']["1027"]['quotes']['USD']['price']
+
+            usd = convert_ethereum_to_usd(amount)
+            eth = amount
+            
+            return dict(usd=usd, eth=eth)
