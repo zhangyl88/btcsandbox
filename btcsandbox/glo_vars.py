6d1fbe859c9e28206281548b8710b33f9f04e9e9
unknown <dycode999@gmail.com>
2022-08-01 19:59:05 +0800

    Modified for DB Migrations

--- btcsandbox/glo_vars.py
+++ btcsandbox/glo_vars.py
@@ -2,3 +2,6 @@ from account.models import Account
 
 emails                          = [i.email for i in Account.objects.all()]
 usernames                       = [i.username for i in Account.objects.all()]
+
+# emails                            = []
+# usernames                         = []
